17d5cf7eae56aca3f1a037738c7c7990d380e6a9
이예은 <127955610+yeeunli@users.noreply.github.com>
2025-10-24 09:48:29 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

--- Python3/프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
+++ Python3/프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -4,7 +4,7 @@ def solution(x1, x2, x3, x4):
     return logical_and(logical_or(x1, x2), logical_or(x3, x4))
 
 def logical_or(a, b):
-    return (a or b)
+    return (a | b)
 
 def logical_and(a, b):
-    return (a and b)
+    return (a & b)
