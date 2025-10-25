b011ef8d1b52e1c071d4ca732561f512e8f63697
이예은 <127955610+yeeunli@users.noreply.github.com>
2025-10-25 23:30:52 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 9.16 MB -BaekjoonHub

--- Python3/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ Python3/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,3 +1,9 @@
 def solution(my_string, s, e):
     
-    return my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
+    reverse_word = reversed(my_string[s:e+1])
+    
+    # reversed() 함수는 결과를 반환하지 않기 때문에 join()으로 문자열로 변환
+    # 같은 타입끼리 + 연산 가능해짐
+    return my_string[:s] + ''.join(reverse_word) + my_string[e+1:]
+
+    # return my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
