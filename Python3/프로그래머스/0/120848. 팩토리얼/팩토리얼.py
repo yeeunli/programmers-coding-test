2201a46f2de14f883479e21ecd8497972ab41a3a
이예은 <127955610+yeeunli@users.noreply.github.com>
2025-10-26 00:09:56 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 9.35 MB -BaekjoonHub

--- Python3/프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ Python3/프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,11 +1,11 @@
-import math
+from math import factorial
 
 def solution(n):
     answer = 0
     num = []
     
     for i in range(1, 11):
-        if math.factorial(i) <= n:
+        if factorial(i) <= n:
             num.append(i)
     
     answer = sorted(num)[-1]
