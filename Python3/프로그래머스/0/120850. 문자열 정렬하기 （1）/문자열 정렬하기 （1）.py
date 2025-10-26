4c36a4fd605e8b3d01797c5c44f764ac56bf41ee
이예은 <127955610+yeeunli@users.noreply.github.com>
2025-10-26 18:57:04 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.02 ms, Memory: 9.36 MB -BaekjoonHub

--- Python3/프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ Python3/프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,10 +1,15 @@
 def solution(my_string):
     answer = []
     
+    # 문자열에 숫자만 있는가 - isdecimal() ⊂ isdigit() ⊂ isnumeric()
+            # isdecimal() - int()로 변환 가능한 기본 숫자(0-9)
+            # isdigit() - 숫자처럼 생겼다면 다 가능 ex. 제곱/첨자 숫자
+            # isnumeric() - 숫자를 나타내는 문자도 가능 ex. 분수, 로마 숫자 등 숫자 값을 나타내는 모든 유니코드 문자
+    # 문자열에 문자만 있는가 - isalpha()
+    # 문자열이 수와 문자로 구성되어 있는가 - isalnum()
+    
     for i in my_string:
-        if i in '1234567890':
+        if i.isdigit():
             answer.append(int(i))
             
-    answer = sorted(answer)
-    
-    return answer
+    return sorted(answer)
