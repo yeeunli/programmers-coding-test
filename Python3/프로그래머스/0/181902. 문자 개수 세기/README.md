dd135ac1d3cd9017547119f69cf240022a3627f0
이예은 <127955610+yeeunli@users.noreply.github.com>
2025-10-26 21:36:54 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.15 ms, Memory: 9.11 MB -BaekjoonHub

--- Python3/프로그래머스/0/181902. 문자 개수 세기/README.md
+++ Python3/프로그래머스/0/181902. 문자 개수 세기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자 개수 세기 - 181902 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181902) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181902#) 
 
 ### 성능 요약
 
-메모리: 9.1 MB, 시간: 0.36 ms
+메모리: 9.11 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 26일 21:26:44
+2025년 10월 26일 21:36:52
 
 ### 문제 설명
 
--- Python3/프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ Python3/프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,18 +1,27 @@
-from collections import Counter
+# from collections import Counter
 
 def solution(my_string):
-    answer = [0 for i in range(52)]
-    arr_num = []
+    answer = [0] * 52
     
-    for i in range(len(my_string)):
-        if 'a' <= my_string[i] <= 'z':
-            arr_num.append(ord(my_string[i])-97+26)
-        if 'A' <= my_string[i] <= 'Z':
-            arr_num.append(ord(my_string[i])-65)
-    
-    arr_num = Counter(arr_num).most_common()
-    
-    for k, v in arr_num:
-        answer[k] = v
-        
+    for i in my_string:
+        if i.isupper():
+            answer[ord(i)-65] += 1
+        else:
+            answer[ord(i)-71] += 1
+            
     return answer
+#     answer = [0 for i in range(52)]
+#     arr_num = []
+    
+#     for i in range(len(my_string)):
+#         if 'a' <= my_string[i] <= 'z':
+#             arr_num.append(ord(my_string[i])-97+26)
+#         if 'A' <= my_string[i] <= 'Z':
+#             arr_num.append(ord(my_string[i])-65)
+    
+#     arr_num = Counter(arr_num).most_common()
+    
+#     for k, v in arr_num:
+#         answer[k] = v
+        
+#     return answer
