14bec2cde0d3666542400f32c67925ca0fc9e106
이예은 <127955610+yeeunli@users.noreply.github.com>
2025-10-27 15:56:31 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.35 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = ''
+    
+    for i in my_string:
+        if i.isupper():
+            answer += chr(ord(i) + 32)
+        else:
+            answer += chr(ord(i) - 32)
+    
+    return answer
