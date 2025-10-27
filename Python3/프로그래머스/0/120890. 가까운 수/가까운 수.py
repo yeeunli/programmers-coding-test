e4ed2e6c33d4c40b018ec739e879d8c1d422c4b1
이예은 <127955610+yeeunli@users.noreply.github.com>
2025-10-27 15:43:30 +0900

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 9.19 MB -BaekjoonHub

--- Python3/프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ Python3/프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,15 +1,26 @@
-def solution(array, n):
-    answer = []
-    result = 0
+# def solution(array, n):
+#     answer = []
+#     result = 0
     
-    for i in array:
-        answer.append(abs(i - n))
+#     for i in array:
+#         answer.append(abs(i - n))
     
-    i = answer.index(min(answer))
-    result = array[i]
+#     i = answer.index(min(answer))
+#     result = array[i]
     
-    for k in range(i+1, len(answer)):
-        if min(answer) == answer[k]:
-            result = min(array[i], array[k])
+#     for k in range(i+1, len(answer)):
+#         if min(answer) == answer[k]:
+#             result = min(array[i], array[k])
             
-    return result
+#     return result
+
+def solution(array, n):
+    
+    # List Comprehension: [ (결과 표현식) (반복문) ]
+    tmp = [(abs(num-n), num) for num in array]
+    
+    # 첫 번째 요소를 기준으로 정렬
+    # but, 같다면 두 번째 요소로 정렬
+    tmp.sort()
+    
+    return tmp[0][1]
