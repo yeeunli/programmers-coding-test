aebe22c154dc1e58e6221dfa59646d7ec63d77d0
이예은 <127955610+yeeunli@users.noreply.github.com>
2025-10-27 15:01:11 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 12.29 ms, Memory: 9.54 MB -BaekjoonHub

--- Python3/프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
+++ Python3/프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
@@ -1,7 +1,13 @@
 def solution(arr, queries):
     
+    # for s, e in queries:
+    #     for i in range(len(arr)):
+    #         if s <= i <= e:
+    #             arr[i] += 1
+    
+    # 배수든 범위든 우선 슬라이싱을 생각하자
     for s, e in queries:
-        for i in range(len(arr)):
-            if s <= i <= e:
-                arr[i] += 1
+        for i in range(s, e+1):
+            arr[i] += 1
+                
     return arr
