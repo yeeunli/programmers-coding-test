409eec07c04fbda74fa62cd540a9eee516bb9e4b
이예은 <127955610+yeeunli@users.noreply.github.com>
2025-10-27 15:59:27 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 9.15 MB -BaekjoonHub

--- Python3/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ Python3/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,12 +1,15 @@
 def solution(my_string):
-    answer = ''
     
-    for i in my_string:
-        if i.isupper():
-            # answer += chr(ord(i) + 32)
-            answer += i.lower()
-        else:
-            # answer += chr(ord(i) - 32)
-            answer += i.upper()
+    return my_string.swapcase()
     
-    return answer
+#     answer = ''
+    
+#     for i in my_string:
+#         if i.isupper():
+#             # answer += chr(ord(i) + 32)
+#             answer += i.lower()
+#         else:
+#             # answer += chr(ord(i) - 32)
+#             answer += i.upper()
+    
+#     return answer
